cda4c66fdbe2493840fff267a0e9b934a89b9728
ness727 <pinpanel237@gmail.com>
2025-10-03 20:01:37 +0900

    프로젝트 시작

---
+++ src/main/java/com/megamaker/myportalnewsbatch/MyPortalNewsBatchApplication.java
@@ -0,0 +1,13 @@
+package com.megamaker.myportalnewsbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPortalNewsBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPortalNewsBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/megamaker/myportalnewsbatch/MyPortalNewsBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megamaker.myportalnewsbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortalNewsBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
